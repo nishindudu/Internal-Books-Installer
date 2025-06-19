d996355809fd1150478f166a4d60953fd5acdfb2
Nishin Santhosh <nishindudu@gmail.com>
2025-06-20 00:05:13 +0530

    Added EULA

--- install.bat
+++ install.bat
@@ -1,6 +1,20 @@
 @echo off
 echo Installing InternalBooks...
 
+
+echo.
+echo ============= END USER LICENSE AGREEMENT =============
+type EULA.md
+echo ======================================================
+echo.
+
+set /p agree="Do you agree to the terms of the EULA? (yes/no): "
+if /i "%agree%" NEQ "yes" (
+    echo You must agree to the EULA to proceed with the installation.
+    echo Installation aborted.
+    exit /b 1
+)
+
 docker pull nishindudu/internalbooks:latest
 if %ERRORLEVEL% NEQ 0 (
     echo Failed to pull the InternalBooks Docker image. Error code: %ERRORLEVEL%
