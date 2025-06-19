f09f911748fe4aca757bc007bf40be3d7fbc4dc7
Nishin Santhosh <nishindudu@gmail.com>
2025-06-19 21:41:17 +0530

    Fixed download link

--- main.js
+++ main.js
@@ -1,7 +1,13 @@
 async function renderEULA() {
     const eulaText = document.querySelector('.eula-text');
-    const eulaContent = await fetch('eula.md');
+    const eulaContent = await fetch('/EULA.md');
     eulaText.innerHTML = marked.parse(await eulaContent.text());
 }
 
 renderEULA();
+
+const acceptButton = document.querySelector('#accept-eula');
+acceptButton.addEventListener('change', () => {
+    const downloadButton = document.querySelector('#dwnldbtn');
+    downloadButton.disabled = !acceptButton.checked;
+});
